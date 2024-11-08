8e50610e61980189ca840d907357aa7f92f7dc97
Your Name <you@example.com>
2024-11-08 19:11:46 +0900

    Complete project

--- app2.py
+++ app2.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    print(request.method, "↓")
-    return "Login Page"
+    # print(request.method)
+    if request.method == "GET":
+
+        return """
+        <form action="/login" method="post">
+        password<input type="password">
+        <br>
+        <input type="submit">
+        </form>
+"""
+    else:
+        return "Logged in"
 
 
 if __name__ == "__main__":
